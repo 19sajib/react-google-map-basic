c11256892215f9ab0f0d9257d400ee66193e0a60
Sajib <contact2sajib@gmail.com>
2021-04-14 10:03:51 +0600

    New Working Example

---
+++ src/Renderdirection/MapDirection.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    DirectionsRenderer,
+} from "react-google-maps";
+
+const MapDirection = ({ origin2, destination2 }) => {
+    const [directions, setDirections] = useState(null)
+
+    useEffect(() => {
+        const google = window.google
+        const directionsService = new google.maps.DirectionsService();
+ 
+     const origin = { lat: 23.6238, lng: 90.5000};
+     const destination = { lat: 23.8103, lng:  90.4125 }
+ 
+     directionsService.route(
+         {
+            origin: new google.maps.LatLng(23.6238, 90.5000),
+            destination: new google.maps.LatLng(23.8103, 90.4125),
+             travelMode: google.maps.TravelMode.DRIVING,
+             
+         },
+         (result, status) => {
+             if (status === google.maps.DirectionsStatus.OK) {
+                 console.log(result)
+                 setDirections(result)
+             } else {
+                 console.error(`error fetching directions ${result}`);
+             }
+         }
+     );
+    }, [])
+    const MapLoader = withScriptjs(
+        withGoogleMap(props => (
+            <GoogleMap
+                defaultCenter={{ lat: 23.8103, lng:  90.4125 }}
+                defaultZoom={17}
+                defaultOptions={{
+                    streetViewControl: true, 
+                    fullscreenControl: true, 
+                    mapTypeControl: true,
+                    zoomControl: true,           
+             }}
+            >
+            {directions && <DirectionsRenderer directions={directions}/>}
+            </GoogleMap> )
+    ) )
+
+  return (
+
+<div className="App">
+<MapLoader
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key="
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px`, width: "500px" }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+  />
+</div>
+  );
+}
+
+export default MapDirection
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactAutocomplete
+    <App
      />
   </React.StrictMode>,
   document.getElementById('root')
