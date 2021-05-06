68be82e98c4860d358427b034e941650a5c6cfee
Sajib <58961853+2sajib2@users.noreply.github.com>
2021-05-06 15:36:44 +0600

    material ui test

--- src/Autocomplete/ReactAutocomplete.js
+++ src/Autocomplete/ReactAutocomplete.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import Autocomplete from 'react-google-autocomplete';
+import Autocomplete, { usePlacesWidget }  from 'react-google-autocomplete';
 
 const ReactAutocomplete = () => {
+        const { ref: materialRef } = usePlacesWidget({
+        apiKey: "API_Key",
+        onPlaceSelected: (place) => console.log(place),
+        inputAutocompleteValue: "country",
+        options: {
+          componentRestrictions: { country: "bd" }
+        },
+      });
     return (
-
+    
+    <TextField
+    fullWidth  
+    label="enter your location"
+    color="secondary"
+    variant="outlined"
+    inputRef={materialRef}
+    />
     <Autocomplete
     style={{width: '90%'}}
     onPlaceSelected={(place) => {
